e898238411777693fae311d6f54a35e919ea2c09
sisshmue <sisshmue404@gmail.com>
2026-02-13 11:54:23 +0700

    add cart to notFound page

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,11 +1,11 @@
 import { Header } from "../components/Header";
 import './NotFoundPage.css'
 
-export function NotFoundPage() {
+export function NotFoundPage({cart}) {
   return (
     <>
       <title>Page Not Found</title>
-      <Header></Header>
+      <Header cart={cart}></Header>
       <div className="not-found-page">
         <p>Page Not Found!</p>
       </div>
